b101a7eccac1173d297bf7753b19ae2a5a41b6d1
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-12 11:26:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let nums = [2,4,6,7,8,9,10,11]
+
+let mevalar = [
+    "olma" ,
+    "anor" ,
+    "banan",
+    "nok",
+    "gilos",
+    "olcha",
+    "uzum",
+    "apelsin",
+    "shaftoli",
+    "qulupnay", 
+]
+
+for (const child of mevalar) 
+    if (child.includes('a')) {
+        console.log(child + '  mevasida a harfi bor' );
+    }
+    
+
+
+
